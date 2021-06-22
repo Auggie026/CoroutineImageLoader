89c64c6b21f45026cf6773772550794cad38aa33
Augusto Castro <augustocesarcastro026@gmail.com>
2021-06-22 19:36:27 -0300

    added pictures and crossfade

--- app/src/main/java/com/example/coroutineimageloader/MainActivity.kt
+++ app/src/main/java/com/example/coroutineimageloader/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.coroutineimageloader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import coil.load
+import com.example.coroutineimageloader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.image1.load(R.drawable.image1){
+            crossfade(true)
+            crossfade(1000)
+        }
+        binding.image2.load(R.drawable.image2){
+            crossfade(true)
+            crossfade(1000)
+        }
     }
 }
