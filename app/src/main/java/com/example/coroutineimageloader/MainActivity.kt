38dc8bed16eb1a0a2643fcf596aebad3f9826624
Augusto Castro <augustocesarcastro026@gmail.com>
2021-06-22 19:58:28 -0300

    finished project. Image loader from assets and from internet link

--- app/src/main/java/com/example/coroutineimageloader/MainActivity.kt
+++ app/src/main/java/com/example/coroutineimageloader/MainActivity.kt
@@ -3,24 +3,36 @@ package com.example.coroutineimageloader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import coil.load
+import coil.transform.CircleCropTransformation
+import coil.transform.GrayscaleTransformation
+import coil.transform.RoundedCornersTransformation
 import com.example.coroutineimageloader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val link = "https://i0.wp.com/volatileinnovation.com/wp-content/uploads/2020/09/landscape-mountains-sky-4843193.jpg"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.image1.load(R.drawable.image1){
+        binding.image1.load(link){
+            placeholder(R.drawable.ic_launcher_background)
             crossfade(true)
-            crossfade(1000)
+            crossfade(500)
+            transformations(
+                    CircleCropTransformation(),
+                    GrayscaleTransformation()
+
+                )
         }
         binding.image2.load(R.drawable.image2){
             crossfade(true)
             crossfade(1000)
+            transformations(RoundedCornersTransformation(100f))
+            transformations(CircleCropTransformation())
         }
     }
 }
